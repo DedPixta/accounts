201b87c6c542dee4bbe4183b3629bdc9abeeeb7a
Kossatyy Maxim <maxim.k@halykmarket.com>
2024-09-03 14:29:34 +0500

    init

---
+++ src/test/java/dev/makos/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.makos.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
